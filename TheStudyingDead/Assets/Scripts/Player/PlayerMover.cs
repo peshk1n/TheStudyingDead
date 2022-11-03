b722ec6b071fea65184d70e17c3c0c259dd621a4
stashaKaktus <anna_konoplyanova@mail.ru>
2022-11-03 14:51:58 +0300

    Update Player mover

--- TheStudyingDead/Assets/Scripts/Player/PlayerMover.cs
+++ TheStudyingDead/Assets/Scripts/Player/PlayerMover.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveSpeed = 3;
+
+    private PlayerInput _playerInput;
+    private string _directionState = DOWN;
+    private Animator _animatorController;
+
+    private const string UP = "Up";
+    private const string DOWN = "Down";
+    private const string RIGHT = "Right";
+    private const string LEFT = "Left";
+
+    private void Awake()
     {
-        
+        _playerInput = new PlayerInput();
+        _animatorController = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    private void Update()
+    {
+        Vector2 moveDirection = _playerInput.Player.Move.ReadValue<Vector2>();
+        transform.Translate(moveDirection * _moveSpeed * Time.deltaTime);
+
+        if (moveDirection == Vector2.zero)
+            Idle();
+        else if (moveDirection.y == 1)
+            Move(UP);
+        else if (moveDirection.y == -1)
+            Move(DOWN);
+        else if (moveDirection.x == 1)
+            Move(RIGHT);
+        else if (moveDirection.x == -1)
+            Move(LEFT);
+    }
+
+    private void Move(string direction)
+    {
+        if (direction == _directionState)
+            return;
+
+        _animatorController.Play($"Move {direction}");
+        _directionState = direction;
+    }
+
+    private void Idle()
+    {
+        _animatorController.Play($"Idle {_directionState}");
     }
 }
