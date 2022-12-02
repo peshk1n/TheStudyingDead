b30ba6b33a10071e10f018874a94303e8604ef9c
Anastasia <keleshyan.nastya@bk.ru>
2022-12-02 12:01:06 +0300

    Add quick fix

---
+++ TheStudyingDead/Assets/Scripts/UI/Inventory/Health/HealthBar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Image _bar;
+
+    public void SetProgress(float progress)
+    {
+        _bar.fillAmount = progress;
+    }
+}
