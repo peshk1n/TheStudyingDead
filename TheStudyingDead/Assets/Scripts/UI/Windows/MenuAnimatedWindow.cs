e3ef62d119bb1a325173fd231fe5f5da559a935d
Anastasia <keleshyan.nastya@bk.ru>
2022-12-04 00:46:37 +0300

    Add menu functional

--- TheStudyingDead/Assets/Scripts/UI/Windows/MenuAnimatedWindow.cs
+++ TheStudyingDead/Assets/Scripts/UI/Windows/MenuAnimatedWindow.cs
@@ -1,19 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MenuAnimatedWindow : AnimatedWindow
+public class MenuAnimatedWindow : MonoBehaviour
 {
-    private Canvas _menuCanvas;
+    private CanvasGroup _panel;
+    private Animator _animator;
+    private Action _closeAction;
 
     void Start()
     {
-        _menuCanvas = GetComponent<Canvas>();
-        _menuCanvas.enabled = false;
+        _panel= GetComponent<CanvasGroup>();
+        _animator = GetComponent<Animator>();
+        _animator.SetTrigger("Normal");
+    }
+
+    public void Close()
+    {
+        _animator.SetTrigger("Close");
     }
 
     public void OpenMenu()
     {
-        _menuCanvas.enabled = !_menuCanvas.enabled;
+        if (_panel.alpha == 1)
+            Close();
+        else _animator.SetTrigger("Show");
+    }
+
+    public void OnContinue()
+    {
+        Close();
+    }
+
+    public void OnStartNewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Close();
+    }
+
+    public void OnShowControl()
+    {
+        var _canvas = _panel.GetComponentInParent<Canvas>();
+        var panel = Resources.Load<GameObject>("UI/ControlPanel");
+        Instantiate(panel, _canvas.transform);
+    }
+
+    public void OnShowOptions()
+    {
+        var _canvas = _panel.GetComponentInParent<Canvas>();
+        var panel = Resources.Load<GameObject>("UI/OptionsPanel");
+        Instantiate(panel, _canvas.transform);
+    }
+
+    public void OnShowMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+        Close();
+    }
+
+    public void OnExit()
+    {
+        _closeAction = () =>
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            OpenMenu();
+
+            Close();
+        };
+    }
+
+    public void OnCloseAnimationComplete()
+    {
+        Destroy(gameObject);
+        _closeAction?.Invoke();
     }
 }
