3427c5cf22bf680ccb3c62e4335003cb4bc09e67
Anastasia <keleshyan.nastya@bk.ru>
2022-12-04 20:54:54 +0300

    quick fix

--- TheStudyingDead/Assets/Scripts/UI/Inventory/Slot.cs
+++ TheStudyingDead/Assets/Scripts/UI/Inventory/Slot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem.Android;
+//using UnityEngine.InputSystem.Android;
 using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
