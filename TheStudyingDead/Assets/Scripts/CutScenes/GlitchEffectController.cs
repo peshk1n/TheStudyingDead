90bf32f86dce506dea16bddc1c4981e35bc58a96
stashaKaktus <anna_konoplyanova@mail.ru>
2023-05-07 12:38:30 +0300

    Add Glitch Screen Controller

---
+++ TheStudyingDead/Assets/Scripts/CutScenes/GlitchEffectController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlitchEffectController : MonoBehaviour
+{
+    [SerializeField] Material _screenMaterial;
+    [SerializeField] Player _player;
+
+    public void SetParameters()
+    {
+        float hp = (float)_player.CurHealth / (float)_player.MaxHealth;
+
+        /*        _screenMaterial.SetFloat("_Switch", 1);
+                _screenMaterial.SetFloat("_ScanLinesStrength", hp);*/
+
+        if (hp >= 9)
+        {
+            DisableGlitchEffect();
+            return;
+        }
+
+        _screenMaterial.SetFloat("_NoiseAmount", 50);
+
+        _screenMaterial.SetFloat("_VignetteRadius", Mathf.Lerp(0.5f, 0.8f, hp));
+        _screenMaterial.SetFloat("_ScanLinesStrength", hp);
+
+        if (hp <= 0.6)
+        {
+            _screenMaterial.SetFloat("_GlitchStrength", 1);
+        }
+    }
+
+    public void EnableGlitchEffect()
+    {
+        _screenMaterial.SetFloat("_Switch", 0.2f);
+
+        _screenMaterial.SetFloat("_NoiseAmount", 0);
+        _screenMaterial.SetFloat("_GlitchStrength", 0);
+        _screenMaterial.SetFloat("_ScanLinesStrength", 1);
+        _screenMaterial.SetFloat("_VignetteRadius", 1);
+        _screenMaterial.SetFloat("_VignetteSoftness", 0.5f);
+    }
+
+    public void DisableGlitchEffect()
+    {
+        EnableGlitchEffect();
+        _screenMaterial.SetFloat("_Switch", 0);
+    }
+
+    public void OnDisable()
+    {
+        DisableGlitchEffect();
+    }
+
+    public void ShowDeathScreenEffect()
+    {
+        _screenMaterial.SetFloat("_Switch", 1);
+
+        _screenMaterial.SetFloat("_NoiseAmount", 30);
+        _screenMaterial.SetFloat("_GlitchStrength", 1);
+        _screenMaterial.SetFloat("_ScanLinesStrength", 0);
+        _screenMaterial.SetFloat("_VignetteRadius", 1);
+        _screenMaterial.SetFloat("_VignetteSoftness", 0.5f);
+    }
+}
