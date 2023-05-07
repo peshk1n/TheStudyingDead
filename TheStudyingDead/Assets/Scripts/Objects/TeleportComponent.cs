18afc8fa7dc503481603841d85f9290a6feaac01
Anastasia <keleshyan.nastya@bk.ru>
2023-05-07 03:39:24 +0300

    Add Locations

--- TheStudyingDead/Assets/Scripts/Objects/TeleportComponent.cs
+++ TheStudyingDead/Assets/Scripts/Objects/TeleportComponent.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class TeleportComponent : MonoBehaviour
 {
     [SerializeField] private Transform _destTransform;
+    [SerializeField] private string _keyId;
+    private Player _player;
+
+
+    private void Start()
+    {
+        _player = FindObjectOfType<Player>();
+    }
 
     public void Teleport()
     {
-        var player=GameObject.Find("Player");
+        var player = GameObject.Find("Player");
         player.transform.position = _destTransform.position;
     }
+
+    public void OpenClosedDoor()
+    {
+        var inventory = _player.GetComponent<Inventory>();
+        if (inventory)
+        {
+            if (inventory.ContainId(_keyId))
+                Teleport();
+        }
+    }
+    
 }
